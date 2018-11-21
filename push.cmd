5d2d74fb46ffa261345caec6995f28f9ac6ee2ce
Nate Zaugg <Nate@MindfireTechnology.com>
2018-11-21 12:53:49 -0700

    Fix for working with Windows Server 2016

---
+++ push.cmd
@@ -0,0 +1,6 @@
+docker-compose build --no-cache
+
+docker push docker.dev.mfire.co/mindfire.dotnet_sdk.node:latest
+docker push docker.dev.mfire.co/mindfire.dotnet_sdk.node:latest_win2016
+docker push docker.dev.mfire.co/mindfire.dotnet_runtime.node:latest
+docker push docker.dev.mfire.co/mindfire.dotnet_runtime.node:latest_win2016
