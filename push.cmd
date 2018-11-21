d0b83534fd8b9d0a6e6fb0e100130ac6d328335f
Daniel Beus <dbeus@mindfiretechnology.com>
2018-11-21 16:20:46 -0700

    Updated to better indicate builds and tags for specific versions of .NET and Windows

--- push.cmd
+++ push.cmd
@@ -1,6 +1,16 @@
-docker-compose build --no-cache
+docker-compose -f dotnet/dotnet2.1/docker-compose.yml build
+docker-compose -f dotnet/dotnet2.2/docker-compose.yml build
 
-docker push docker.dev.mfire.co/mindfire.dotnet_sdk.node:latest
-docker push docker.dev.mfire.co/mindfire.dotnet_sdk.node:latest_win2016
-docker push docker.dev.mfire.co/mindfire.dotnet_runtime.node:latest
-docker push docker.dev.mfire.co/mindfire.dotnet_runtime.node:latest_win2016
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk.nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-runtime
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-runtime-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-aspnetcore
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-aspnetcore-nanoserver
+
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-sdk
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-sdk.nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-runtime
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-runtime-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore-nanoserver
