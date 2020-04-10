15aaceb233742a7b7d836552391af0169a3fe60c
Nate Zaugg <Nate@MindfireTechnology.com>
2020-04-10 11:16:59 -0600

    - Fixed build issues for 2019 containers - Added support for Core 2.1 Linux Containers

--- push-linux.cmd
+++ push-linux.cmd
@@ -1,11 +1,21 @@
+docker-compose -f dotnet/dotnet2.1/docker-compose-linux.yml build
 docker-compose -f dotnet/dotnet3.1/docker-compose-linux.yml build
+
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk-alpine
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-runtime-alpine
+docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-aspnetcore-alpine
 
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk-alpine
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-alpine
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-alpine
 
+
 REM PUSHING TO GITHUB -- break script now if you do not wish to do this
 timeout 30
+
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk-alpine mindfiretech/dotnet.node:2.1-sdk-alpine
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:2.1-runtime-alpine mindfiretech/dotnet.node:2.1-runtime-alpine
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:2.1-aspnetcore-alpine mindfiretech/dotnet.node:2.1-aspnetcore-alpine
 
 docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk-alpine mindfiretech/dotnet.node:3.1-sdk-alpine
 docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-alpine mindfiretech/dotnet.node:3.1-runtime-alpine
@@ -13,6 +23,10 @@ docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-alpine mindfi
 
 REM Tag Latest
 docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-alpine mindfiretech/dotnet.node:latest
+
+docker push mindfiretech/dotnet.node:2.1-sdk-alpine
+docker push mindfiretech/dotnet.node:2.1-runtime-alpine
+docker push mindfiretech/dotnet.node:2.1-aspnetcore-alpine
 
 docker push mindfiretech/dotnet.node:3.1-sdk-alpine
 docker push mindfiretech/dotnet.node:3.1-runtime-alpine
