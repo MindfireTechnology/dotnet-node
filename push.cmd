877e92295ae71a4d4f1a9cb2f43325ae9dd1bdd6
Nate Zaugg <Nate@MindfireTechnology.com>
2022-02-21 14:17:16 -0700

    Merge Chris' stuff and made some modifications
    Signed-off-by: Nate Zaugg <Nate@MindfireTechnology.com>

--- push.cmd
+++ push.cmd
@@ -1,33 +1,39 @@
+docker-compose -f dotnet/dotnet2.1/docker-compose.yml build
+docker-compose -f dotnet/dotnet2.2/docker-compose.yml build
 docker-compose -f dotnet/dotnet3.1/docker-compose.yml build
 docker-compose -f dotnet/dotnet6.0/docker-compose.yml build
-docker-compose -f dotnet/dotnet3.1/docker-compose.yml build
 
-docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk
+
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk-nanoserver-2019
-docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime
-docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-runtime-nanoserver-2019
-docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore
-docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-aspnetcore-nanoserver-2019
 
-docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-sdk-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-sdk-nanoserver-2019
-docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime
-docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-runtime-nanoserver-2019
-docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore
-docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore-nanoserver-2019
 
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk-nanoserver-2019
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-nanoserver-2019
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-nanoserver
 docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-nanoserver-2019
+
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk-win10
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk-2019
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime-win10
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime-2019
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore-win10
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore-2019
+
 
 REM PUSHING TO GITHUB -- break script now if you do not wish to do this
 timeout 30
@@ -58,6 +64,16 @@ docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-nanoserver mindf
 docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-nanoserver-2019 mindfiretech/dotnet.node:3.1-runtime-nanoserver-2019
 docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-nanoserver mindfiretech/dotnet.node:3.1-aspnetcore-nanoserver
 docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-nanoserver-2019 mindfiretech/dotnet.node:3.1-aspnetcore-nanoserver-2019
+
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk mindfiretech/dotnet.node:6.0-sdk
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk-win10 mindfiretech/dotnet.node:6.0-sdk-win10
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk-2019 mindfiretech/dotnet.node:6.0-sdk-2019
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime mindfiretech/dotnet.node:6.0-runtime
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime-win10 mindfiretech/dotnet.node:6.0-runtime-win10
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime-2019 mindfiretech/dotnet.node:6.0-runtime-2019
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore mindfiretech/dotnet.node:6.0-aspnetcore
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore-win10 mindfiretech/dotnet.node:6.0-aspnetcore-win10
+docker tag docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore-2019 mindfiretech/dotnet.node:6.0-aspnetcore-2019
 
 REM Tag Latest
 docker tag docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore mindfiretech/dotnet.node:latest
@@ -98,3 +114,14 @@ docker pull docker.dev.mfire.co/mindfire.dotnet.node:2.2-runtime-nanoserver-2019
 docker pull docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore
 docker pull docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore-nanoserver
 docker pull docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore-nanoserver-2019
+
+
+docker push mindfiretech/dotnet.node:6.0-sdk
+docker push mindfiretech/dotnet.node:6.0-sdk-win10
+docker push mindfiretech/dotnet.node:6.0-sdk-2019
+docker push mindfiretech/dotnet.node:6.0-runtime
+docker push mindfiretech/dotnet.node:6.0-runtime-win10
+docker push mindfiretech/dotnet.node:6.0-runtime-2019
+docker push mindfiretech/dotnet.node:6.0-aspnetcore
+docker push mindfiretech/dotnet.node:6.0-aspnetcore-win10
+docker push mindfiretech/dotnet.node:6.0-aspnetcore-2019
