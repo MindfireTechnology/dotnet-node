4a2f3b86df6c42cb9e91ae554fc59cb1b98103c8
Christopher Billingsley <cbillingsley@mindfiretechnology.com>
2022-02-21 11:15:49 -0700

    Added Docker images using node with .NET Core 3.1 and 6.0 for Server 2019 and 2022 + alpine.

--- push.cmd
+++ push.cmd
@@ -1,16 +1,22 @@
-docker-compose -f dotnet/dotnet2.1/docker-compose.yml build
-docker-compose -f dotnet/dotnet2.2/docker-compose.yml build
+docker-compose -f dotnet/dotnet3.1/docker-compose.yml build
+docker-compose -f dotnet/dotnet6.0/docker-compose.yml build
 
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-sdk.nanoserver
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-runtime
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-runtime-nanoserver
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-aspnetcore
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.1-aspnetcore-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk.nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-sdk.nanoserver-1809
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-runtime-nanoserver-1809
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:3.1-aspnetcore-nanoserver-1809
 
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-sdk
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-sdk.nanoserver
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-runtime
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-runtime-nanoserver
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore
-docker push docker.dev.mfire.co/mindfire.dotnet.node:2.2-aspnetcore-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk.nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-sdk.nanoserver-1809
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-runtime-nanoserver-1809
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore-nanoserver
+docker push docker.dev.mfire.co/mindfire.dotnet.node:6.0-aspnetcore-nanoserver-1809
